bc9250355434d7847486960e08001aa8691775d4
Oscar Arango <oscar.arango326@gmail.com>
2019-06-04 00:40:04 +0200

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MapasService } from './services/mapas.service';
+import {Marcador} from './interfaces/marcador.interface';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DiriMapas';
+  lat = 38.382538;
+  lng = -0.510123;
+  zoom = 17;
+  marcadorSel: any = null;
+  constructor( public _ms: MapasService) {
+    this._ms.cargarMarcadores();
+  }
+  clickMapa( evento ) {
+    const nuevoMarcador: Marcador = {
+      lat : evento.coords.lat,
+      lng : evento.coords.lng,
+      titulo : 'Sin titulo',
+      draggable : true
+    };
+    this._ms.insertarMarcador(nuevoMarcador);
+    console.log ( evento);
+  }
+
+  clickMarcador( marcador: Marcador, i: number) {
+    console.log(marcador, i );
+    this.marcadorSel = marcador;
+  }
+
+  dragEndMarcador( marcador: Marcador, evento) {
+    const lat = evento.coords.lat;
+    const lng = evento.coords.lng;
+    marcador.lat = lat;
+    marcador.lng = lng;
+    this._ms.guardarMarcadores();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import {AgmCoreModule} from '@agm/core';
+import {MapasService} from './services/mapas.service';
+import {FormsModule} from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyA7YxH9DDYSDNMI3QU1MambRV1sbba9Y98'
+    }),
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    MapasService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  lat = 38.383115;
+  lng = -0.509398;
+}
+
